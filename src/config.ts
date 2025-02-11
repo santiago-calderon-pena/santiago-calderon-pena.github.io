a1767ef9ce59f1f4a89d1c7f09c16e3499f6213b
SantiagoCalderon1999 <santiagocalderon1999@gmail.com>
2025-02-11 13:00:25 -0500

    Add dash in Santiago Calderón-Peña

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Santiago Calderón Peña';
+export const SITE_TITLE = 'Santiago Calderón-Peña';
 export const SITE_DESCRIPTION = 'Software Engineer and Research Assistant';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
